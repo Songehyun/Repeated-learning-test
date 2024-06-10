48cb0462f3f678fbe0c4b4d855be015ae143f657
송이현 <songehyun00@gmail.com>
2024-06-10 11:07:30 +0900

    주석처리 완료

--- work-1.js
+++ work-1.js
@@ -4,30 +4,43 @@ class Person {
     this._age = 0;
     this._job = "";
   }
+  // 클래스 person 만들기 안의 내용들은 비워두고 형식만 지정해주는 느낌
 
   set name(value) {
     this._name = value;
   }
 
+  // name 값을 받아와
+
   get name() {
     return this._name;
   }
+
+  // name을 그대로 출력하기
 
   set age(value) {
     this._age = value;
   }
 
+  // age 값을 받아와
+
   get age() {
     return this._age;
   }
+
+  // age를 그대로 입력해
 
   set job(value) {
     this._job = value;
   }
 
+  // job 값을 받아와
+
   get job() {
     return this._job;
   }
+
+  // job을 그대로 입력해
 }
 
 const person = new Person();
@@ -35,6 +48,10 @@ person.name = "송이현";
 person.age = 31;
 person.job = "공부중";
 
+// 값 입력
+
 console.log("Name:", person.name);
 console.log("Age:", person.age);
 console.log("Job:", person.job);
+
+// console찍어주기
--- work-2.js
+++ work-2.js
@@ -1,21 +1,36 @@
+// createPerson 함수 만들기 , person 객체를 만들어주고 , setName 함수를 불러와줘
+
 const createPerson = (callback) => {
   let person = {};
   callback(person, setName);
 };
+
+// setName 함수 만들기 , setAge 함수를 불러와줘
 
 const setName = (person, callback) => {
   person.name = "송이현";
   callback(person, setAge);
 };
 
+// setAge 함수 만들기 , setJob 함수를 불러와줘
+
 const setAge = (person, callback) => {
   person.age = 31;
   callback(person, setJob);
 };
 
+/**
+ * setJob 함수 만들기
+ * @param {object} person 객체 person안에 내용을 담기 위함
+ * @return person 객체를 console.log 찍어줘
+ */
+
 const setJob = (person) => {
   person.job = "공부중";
   console.log(person);
 };
+
+// createPerson함수를 실행 , person 값을 읽고 callback함수 실행
+// callback함수는 person값을 읽은 setName함수를 실행하는 것
 
 createPerson((person, callback) => callback(person, setName));
